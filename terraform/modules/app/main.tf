5c425e2ee27f33eb6bb76bd6ad8f7b36d6c98d92
bkrasko <bkrasko@cisco.com>
2022-06-11 14:21:10 +0300

    Update Jenkinsfile

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -68,7 +68,7 @@ resource "aws_route53_record" "cert" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = aws_route53_record.cert.*.fqdn
+  validation_record_fqdns = [ for record in aws_route53_record.cert : record.fqdn ]
 }
 
 # NAMESPACES
